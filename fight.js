a563ac37f3c16c0d9d2fe43c2bc0a12d4f44b8ce
fOuttaMyPaint <154358121+fOuttaMyPaint@users.noreply.github.com>
2024-06-24 02:59:26 -0400

    Update fight.js

--- fight.js
+++ fight.js
@@ -1,6 +1,6 @@
 function fight() {
-    let outcome = Math.random();
-    if (outcome < 0.5) {
+    let event = Math.random();
+    if (event < 0.5) {
         let damage = Math.floor(Math.random() * 10) + 1;
         health -= damage;
         logMessage(`You got hit and lost ${damage} HP!`);
@@ -8,7 +8,7 @@ function fight() {
         let exp = Math.floor(Math.random() * 20) + 10;
         experience += exp;
         logMessage(`You defeated an enemy and gained ${exp} experience points!`);
-        levelUp();
     }
+    levelUp();
     updateBars();
 }
