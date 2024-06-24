1cbfcb86fb524f40458cd9603779f264913b4758
fOuttaMyPaint <154358121+fOuttaMyPaint@users.noreply.github.com>
2024-06-24 03:43:13 -0400

    Update progressBar.js

--- progressBar.js
+++ progressBar.js
@@ -1,4 +1,4 @@
 function updateProgressBars() {
-    document.getElementById('health-bar').style.width = `${health}%`;
-    document.getElementById('experience-bar').style.width = `${(experience / (level * 100)) * 100}%`;
+    healthBar.style.width = health + '%';
+    experienceBar.style.width = (experience / (level * 100)) * 100 + '%';
 }
